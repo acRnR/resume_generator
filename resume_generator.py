83b76be5d4385c6a9255a836144e1e4c78f4a8b7
Asya Simonyan <anniesim97@gmail.com>
2017-06-19 23:18:22 +0300

    career + education

--- resume_generator.py
+++ resume_generator.py
@@ -15,7 +15,7 @@ def vk_api(method, **kwargs):
 
 
 def collect_info(u_id):
-    flds = 'first_name,last_name,photo_200,city,career,education,hidden,about,activities,connections,contacts,occupation,personal,site,status'
+    flds = 'first_name,last_name,photo_200,city,career,education,hidden,about,activities,connections,contacts,occupation,personal,site,status,bdate'
     result = vk_api('users.get', user_ids=u_id, fields=flds, v='5.65')
     if 'hidden' not in result['response'][0]:
         clean_info(result['response'][0])
@@ -33,9 +33,22 @@ def clean_info(result):
         u_info['city'] = result['city']['title']
     if 'career' in result and len(result['career']) >= 2:# and 'company' in result['career'][0]:
         #print(len(result['career']))
-        for el in ['company', 'position', 'from', 'to']:
+        for el in ['company', 'position']:
             if el in result['career'][1]:
                 u_info[el] = result['career'][1][el]
+        if 'from' in result['career'][1] and 'to' not in result['career'][1]:
+            u_info['c_date'] = result['career'][1]['from']
+        elif 'from' in result['career'][1] and 'to' in result['career'][1]:
+            u_info['c_date'] = result['career'][1]['from'] + ' - ' + result['career'][1]['to']
+    if 'bdate' in result:
+        u_info['bdate'] = result['bdate']
+    if 'university_name' in result:
+        u_info['uni_name'] = result['university_name']
+        if 'faculty_name' in result and result['faculty_name'] != '':
+            u_info['fac_name'] = result['faculty_name']
+        if 'graduation' in result:
+            u_info['graduation'] = result['graduation']
+
     #if
     print(u_info)
 
@@ -56,6 +69,10 @@ def index():
 @app.route('/result')
 def result():
     if sess['hidden'] != 1:
+        #career = {
+        #    'company':'Организация',
+        #    'c_date':''
+        #}
         kek = u_info
         return render_template('result.html', info=kek)
     else:
